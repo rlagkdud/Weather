2e46a432fa8965b5765daf7c605d1003ca79233c
rlagkdud <ha0936861@gmail.com>
2023-05-15 16:25:37 +0900

    test code 작성해보기

--- src/test/java/zerobase/weather/WeatherApplicationTests.java
+++ src/test/java/zerobase/weather/WeatherApplicationTests.java
@@ -3,11 +3,33 @@ package zerobase.weather;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class WeatherApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void equalTest () {
+	    // given
+	    // when
+	    // then
+		assertEquals(1, 1);
+	}
+
+	@Test
+	void nullTest () {
+	    // given
+	    // when
+	    // then
+		assertNull(null);
+	}
+
+	@Test
+	void trueTest () {
+	    // given
+	    // when
+	    // then
+		assertTrue(1==1);
 	}
 
 }
