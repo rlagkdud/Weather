2d83b339732908f2c52ec48dad8fab1eb04d478f
rlagkdud <ha0936861@gmail.com>
2023-05-18 15:48:59 +0900

    API 문서 작성하기 - Swagger : @ApiOperation, @ApiParam

--- src/main/java/zerobase/weather/config/SwaggerConfig.java
+++ src/main/java/zerobase/weather/config/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("zerobase.weather"))
-                .paths(PathSelectors.ant("/read/**"))
+                .paths(PathSelectors.any())
                 .build().apiInfo(apiInfo());
     }
 
